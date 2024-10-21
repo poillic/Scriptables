85979d98351a93ac8ca42f7f43b9bc9bc0a127ae
Proxyma <oillic.p@gmail.com>
2024-10-21 10:27:50 +0200

    First Commit

---
+++ Variables/IntVariable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( fileName = "new " + nameof( IntVariable ), menuName = ScriptableUtils.VARIABLE_PATH + nameof( IntVariable ) )]
+public class IntVariable : BaseVariable<int>
+{
+}
